f7f8919ba9419caee77b24a0dfcac00805890c45
“yusuke1816” <ktc24a31g0014@edu.kyoto-tech.ac.jp>
2025-04-28 18:34:23 +0900

    pptの文字取得OK

--- backend/app.py
+++ backend/app.py
@@ -47,4 +47,4 @@ def extract_text_from_ppt(file_path):
         return jsonify({'error': str(e)}), 500  # エラーメッセージをクライアントに返す
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
